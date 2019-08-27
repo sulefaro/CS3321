26db7454a57810dd68162c336588903a6e8bf67e
sulefaro <40674251+sulefaro@users.noreply.github.com>
2019-08-27 13:20:22 -0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <fstream>
+#include <fstream> //includes Writing and Reading files
 using namespace std;
 
 int main()
 {
-	ofstream textWrite;
-	textWrite.open("Write.txt");
+	ofstream textFile;
+	textFile.open("Login.txt");//Opens text file called Login.txt
 	
-	//textWrite << "Test Writing.\n";
+	textFile << "Login Information. username and password seperated by tabspace\n"; //text file header
+	textFile << "admin	adminpass\n"; //example of username and password
 	
-	textWrite.close();
-	//cout << "Program ran successfully, output written in text file..." << endl;
-	int q;
-	cin >> q;
+	textFile.close();//Closes the text file
+	cout << "Program ran successfully, output written in text file..." << endl;
 	return 0;
 }
